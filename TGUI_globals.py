db6a6a9dc54db82f9dc4b5b1a312e431a312ff9b
Lutrix1121 <Lukasz.malz@outlook.com>
2025-08-20 21:01:50 +0200

    Add files via upload

---
+++ TGUI_globals.py
@@ -0,0 +1,21 @@
+# Global variables to store application state
+FILENAME = ""
+SAVEPATH = ""
+CATEGORICAL_COLUMNS = []
+
+LIGHT_MODE = {
+    'bg': 'white',
+    'button_bg': 'lightblue',
+    'generate_bg': 'lightgreen',
+    'text': 'black'
+}
+
+DARK_MODE = {
+    'bg': '#2d2d2d',
+    'button_bg': "#848383",
+    'generate_bg': "#669966",
+    'text': 'white'
+}
+
+WIDGETS = []
+CURRENT_THEME = LIGHT_MODE
